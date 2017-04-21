386281d1e7fd2fab52b4a26cdd6887e666e1c401
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 16:11:43 +0300

    Add CardInfoExtensions

---
+++ src/Net.Chdk.Model.Card/CardInfoExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Net.Chdk.Model.Card
+{
+    public static class CardInfoExtensions
+    {
+        public static string GetRootPath(this CardInfo cardInfo)
+        {
+            return $"{cardInfo.DriveLetter}\\";
+        }
+    }
+}
