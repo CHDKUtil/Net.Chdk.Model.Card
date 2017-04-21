04f6e603d46636cd2802c3311446b9eac49f6eec
Dmitry Shechtman <damnian@gmail.com>
2017-04-21 16:23:22 +0300

    Add GetMetadataPath()

--- src/Net.Chdk.Model.Card/CardInfoExtensions.cs
+++ src/Net.Chdk.Model.Card/CardInfoExtensions.cs
@@ -1,10 +1,18 @@
-﻿namespace Net.Chdk.Model.Card
+﻿using System.IO;
+
+namespace Net.Chdk.Model.Card
 {
     public static class CardInfoExtensions
     {
         public static string GetRootPath(this CardInfo cardInfo)
         {
             return $"{cardInfo.DriveLetter}\\";
+        }
+
+        public static string GetMetadataPath(this CardInfo cardInfo)
+        {
+            var rootPath = GetRootPath(cardInfo);
+            return Path.Combine(rootPath, "METADATA");
         }
     }
 }
