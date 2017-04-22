641f4b0562c8bea76c3dbd8c15fab83f666ff816
Dmitry Shechtman <damnian@gmail.com>
2017-04-22 03:44:44 +0300

    Nullable Capacity and FreeSize

--- src/Net.Chdk.Model.Card/CardInfo.cs
+++ src/Net.Chdk.Model.Card/CardInfo.cs
@@ -6,7 +6,7 @@
         public string DriveLetter { get; set; }
         public string Label { get; set; }
         public string FileSystem { get; set; }
-        public ulong Capacity { get; set; }
-        public ulong FreeSpace { get; set; }
+        public ulong? Capacity { get; set; }
+        public ulong? FreeSpace { get; set; }
     }
 }
